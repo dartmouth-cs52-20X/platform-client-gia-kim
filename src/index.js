6b24c322a3fbee8d36c73d67001e1d869646aab6
Gia Kim <gia.kim.22@dartmouth.edu>
2020-04-17 00:33:18 -0400

    final commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+console.log('starting up');
+
+let num = 0;
+function setInterval() {
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+  num += 1;
+}
+
+setInterval(setInterval, 1000);
