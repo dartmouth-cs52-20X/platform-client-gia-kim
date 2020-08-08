79b7891364d0891ecf02973037d86e6ad1dc4382
Gia Kim <gia.kim.22@dartmouth.edu>
2020-08-08 16:29:59 -0400

    basic functionality working! yay

--- src/reducers/posts-reducer.js
+++ src/reducers/posts-reducer.js
@@ -8,12 +8,13 @@ const initialState = {
 
 // For FETCH_POSTS you would return the state object with the all property set to the new posts.
 // For FETCH_POST return that single post.
+// if that isn’t there then those keys disappear from the reducer which can cause all sorts of bugs
 const PostsReducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionTypes.FETCH_POSTS:
-      return { all: action.payload };
+      return { all: action.payload, current: state.current };
     case ActionTypes.FETCH_POST:
-        return { current: action.payload };
+        return { current: action.payload, all: state.all };
     default:
       return state;
   }
